1d5fe9ccb22c724ac5e4007a66af58028b278699
Trevor Brooks <trevorism@gmail.com>
2023-04-20 12:19:16 -0400

    Try configuration in constructor

--- src/main/groovy/com/trevorism/acceptance/plugin/tasks/AcceptanceTask.groovy
+++ src/main/groovy/com/trevorism/acceptance/plugin/tasks/AcceptanceTask.groovy
@@ -4,10 +4,13 @@ import org.gradle.api.tasks.JavaExec
 
 class AcceptanceTask extends JavaExec {
 
+    AcceptanceTask() {
+        classpath = project.sourceSets.acceptance.runtimeClasspath
+        main = "io.cucumber.core.cli.Main"
+    }
+
     @Override
     void exec() {
-        setClasspath(project.sourceSets.acceptance.runtimeClasspath)
-        setMain("io.cucumber.core.cli.Main")
         args ('--plugin', "json:${project.buildDir.path}/test-results/cucumber/acceptance.json", '--glue', project.acceptanceSettings.stepDefinitionPackageRoot, "src/acceptance/${project.acceptanceSettings.featuresRoot}")
         super.exec()
     }
